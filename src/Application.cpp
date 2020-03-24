27e409fb1c1f6a5dffff00d2aaade76908094d18
Yasen Aleksiev <YAleksiev@nemetschek.bg>
2020-03-25 01:35:20 +0200

    first texture for openGL learing process

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,7 +3,7 @@
 
 
 #include "Renderer.h"
-
+#include "Texture.h"
 #include "IndexBuffer.h"
 #include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
@@ -47,10 +47,10 @@ int main( void )
 	std::cout << glGetString( GL_VERSION ) << std::endl;
 
 	float positions[] = {
-		-0.5f, -0.5f,
-		 0.5f, -0.5f,
-		 0.5f,  0.5f,
-		-0.5f,  0.5f,
+		-0.5f, -0.5f, 0.0f, 0.0f,
+		 0.5f, -0.5f, 1.0f, 0.0f,
+		 0.5f,  0.5f, 1.0f, 1.0f,
+		-0.5f,  0.5f, 0.0f, 1.0f
 	};
 
 	unsigned int indices[] = {
@@ -58,10 +58,14 @@ int main( void )
 		2, 3, 0
 	};
 
+	GLCall( glEnable( GL_BLEND ) );
+	GLCall( glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA) );
+
 	VertexArray va;
-	VertexBuffer vb( positions, 4 * 2 * sizeof( float ) );
+	VertexBuffer vb( positions, 4 * 4 * sizeof( float ) );
 
 	VertexBufferLayout layout;
+	layout.Push<float>( 2 );
 	layout.Push<float>( 2 );
 	va.AddBuffer( vb, layout );
 
@@ -70,6 +74,10 @@ int main( void )
 	Shader shader( "res/shaders/Basic.shader" );
 	shader.Bind();
 	shader.SetUniform4f( "u_Color", 0.8f, 0.3f, 0.8f, 1.0f );
+
+	Texture texture( "res/textures/cppLogo.png" );
+	texture.Bind();
+	shader.SetUniform1i( "u_Texture", 0 );
 
 	va.Unbind();
 	vb.Unbind();
@@ -101,7 +109,7 @@ int main( void )
 		}
 
 		r += increment;
-
+		
 		/* Swap front and back buffers */
 		glfwSwapBuffers( window );
 
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -109,6 +109,16 @@ void Shader::Unbind() const
 	GLCall( glUseProgram( 0 ) );
 }
 
+void Shader::SetUniform1i( const std::string & name, int value )
+{
+	GLCall( glUniform1i( GetUniformLacation( name ), value ) );
+}
+
+void Shader::SetUniform1f( const std::string & name, float value )
+{
+	GLCall( glUniform1f( GetUniformLacation( name ), value ) );
+}
+
 void Shader::SetUniform4f( const std::string & name, float v0, float v1, float v2, float v3 )
 {
 	GLCall( glUniform4f( GetUniformLacation(name), v0, v1, v2, v3 ) );
